f4b9f06d4c27059fe041024e1383915d2770a87a
Gabriella Zurek <gabazurek0222@gmail.com>
2026-06-12 15:31:42 -0500

    Add files via upload
    PS4P1

---
+++ PS4P1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 12 14:53:34 2026
+
+@author: gabaz
+"""
+
+#Code written to display odd numbers (1-25) 
+#Use while loop structure
+
+#identify initial value 
+number=1
+#increase initial value by factors of 2 until reaching 25
+while number <= 25:
+    print(number)
+    number += 2
